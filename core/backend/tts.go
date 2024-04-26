970de1068221caad70526a866f563c6d732bf710
blob42 <contact@blob42.xyz>
2024-04-26 03:59:40 +0200

    TTS endpoint: add optional language paramter

--- core/backend/tts.go
+++ core/backend/tts.go
@@ -29,7 +29,7 @@ func generateUniqueFileName(dir, baseName, ext string) string {
 	}
 }
 
-func ModelTTS(backend, text, modelFile, voice string, loader *model.ModelLoader, appConfig *config.ApplicationConfig, backendConfig config.BackendConfig) (string, *proto.Result, error) {
+func ModelTTS(backend, text, modelFile, voice string, language string, loader *model.ModelLoader, appConfig *config.ApplicationConfig, backendConfig config.BackendConfig) (string, *proto.Result, error) {
 	bb := backend
 	if bb == "" {
 		bb = model.PiperBackend
@@ -83,6 +83,7 @@ func ModelTTS(backend, text, modelFile, voice string, loader *model.ModelLoader,
 		Model: modelPath,
 		Voice: voice,
 		Dst:   filePath,
+		Language: &language,
 	})
 
 	// return RPC error if any
@@ -91,4 +92,10 @@ func ModelTTS(backend, text, modelFile, voice string, loader *model.ModelLoader,
 	}
 
 	return filePath, res, err
+}
+
+func ModelTTSInfo(backend, text, modelFile, voice string, language string, loader *model.ModelLoader, appConfig *config.ApplicationConfig, backendConfig config.BackendConfig) (string, *proto.Result, error) {
+
+
+	return "", nil, nil
 }
